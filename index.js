89d745ac8b414793c34a3756f05fd56e756dc307
arthur_song <arth3rs0ng@gmail.com>
2019-10-10 20:27:47 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => renderBooks(json));
 
+}
+
+function fetchChar(){
+  fetch('https://anapioficeandfire.com/api/characters/1000')
+    .then(resp => resp.json())
+    .then(json => renderChar(json));
+}
+
+function renderChar(json){
+  const main = document.querySelector('main');
+  const h5 = document.createElement('h5');
+  h5.innerHTML = json.name;
+  main.appendChild(h5);
 }
 
 function renderBooks(json) {
@@ -9,8 +25,24 @@ function renderBooks(json) {
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
   })
+  const fifthBook = json[5];
+  const h4 = document.createElement('h4');
+  h4.innerHTML = fifthBook.name;
+  main.appendChild(h4);
+
+  let totalPage = 0;
+  json.forEach(book => {
+    totalPage += book.numberOfPages;
+  })
+  const h5 = document.createElement('h5');
+  h5.innerHTML = `Total pages = ${totalPage}`;
+  main.appendChild(h5);
+
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
+  fetchBooks();
+  fetchChar();
+
 })
