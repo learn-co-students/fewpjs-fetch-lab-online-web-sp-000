3f41b6e56f73f182dd0a2d3ce29fc33b8afbcac5
Tsunamins <reillyamr@gmail.com>
2019-10-16 12:53:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,81 @@
-const select_main = document.querySelector('main');
+//original file contents:
+function fetchBooks() {
+     return fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+   .then(json => renderBooks(json));
+
+}
+
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
+}
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
-  
 })
 
-function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
-  .then(resp => resp.json())
-  .then(result => {
-    const books = result;
-    for (let i=0; i < result.length; i++){
-      const books = books[i];
-      const book_name = books.name;
-      const h2 = document.createElement('h2');
-      h2.textContent = book_name;
-      select_main.appendChild(h2);
-    }
-  });
-}
+
+
+
+
+
+
+
+
+
+
+
+//this works in the consle up to const name = book.name;
+
+//const select_main = document.querySelector('main');
+
+// fetch('https://anapioficeandfire.com/api/books')
+//   .then(resp => resp.json())
+//   .then(result => {
+//     const books = result;
+//     for (let i=0; i < books.length; i++){
+//       const book = books[i];
+//       const name = book.name;
+      
+//       const h2 = document.createElement('h2');
+//       h2.textContent = name;
+//       select_main.appendChild(h2);
+//     }
+//   });
+
+
+
+
+
+//random other things tried and original things tried:
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks()
+//   const select_main = document.querySelector('main');
+// })
+
+// function fetchBooks() {
+//   return fetch('https://anapioficeandfire.com/api/books')
+//   .then(resp => resp.json())
+//   .then(result => {
+//     const books = result;
+//     for (let i=0; i < books.length; i++){
+//       const book = books[i];
+//       const name = book.name;
+//       console.log(name);
+//       const h2 = document.createElement('h2');
+//       h2.textContent = book_name;
+//       select_main.appendChild(h2);
+//     }
+//   });
+// }
+
+// fetchBooks();
 
 
 
