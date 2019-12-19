058ff7591bb35679d373bb37a284ff87f1d12522
chanwkkk <chanwk18@hotmail.com>
2019-12-18 17:48:33 -0700

    update

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
+    h2.innerHTML = `<h2>${book.numberOfPages}</h2>`
     main.appendChild(h2)
   })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
+document.addEventListener('keydown', function() {
+   fetchBooks()
 })
+
+
