ac1b47f9d149ba87e3e9fe0bb0d96077f4a2d773
github username <github email address>
2020-02-06 16:00:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
+  
 }
-
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
@@ -10,6 +13,9 @@ function renderBooks(json) {
     main.appendChild(h2)
   })
 }
+
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
