3b6893db655badd7db070eb00a972f1c67461c77
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-06-25 17:17:18 +0100

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function fetchBooks() {
+// The tests in this lab need access to the fetch() request inside fetchBooks(). In order to give them access,
+//  write you solution so that fetchBooks() returns the fetch(). 
 
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+  // returned response should be converted to JSON. 
+  .then(resp => resp.json()) 
+  //  call the second function, renderBooks(), passing in the API's returned JSON data as the argument.
+  .then(json => renderBooks(json))
 }
 
 function renderBooks(json) {
