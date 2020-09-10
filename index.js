240541a635e868cf17d66b9af16b41c42e324d29
ting682 <tchung682@gmail.com>
2020-09-10 15:39:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function fetchBooks() {
-
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
   const main = document.querySelector('main')
+  //debugger;
   json.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
@@ -11,6 +14,12 @@ function renderBooks(json) {
   })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+function getPosts() {
   fetchBooks()
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  //fetchBooks()
+  getPosts()
+  //window.renderBooks
 })
