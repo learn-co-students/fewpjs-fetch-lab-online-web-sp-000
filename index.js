271968d52ecc041d86c0f2cbb83b2620dfc5ff52
chienleow <chienleow1@gmail.com>
2020-11-17 11:31:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 function fetchBooks() {
-  fetch("https://anapioficeandfire.com/api/books")
+  return fetch("https://anapioficeandfire.com/api/books")
   .then(function(response){
     return response.json();
   })
   .then(function(json){
     renderBooks(json)
-  });
-  return fetch();
+  })
 }
 
 function renderBooks(json) {
