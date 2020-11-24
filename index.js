b46892ffb4a0694f0fe5f0ba251d5a13b666369e
Yumacorn <ReachRyanMui@gmail.com>
2020-11-23 23:19:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
+  return fetch('https://anapioficeandfire.com/api/books', {mode: "no-cors"})
     .then(resp => resp.json())
-    .then(json => console.log(json));
+    .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
+  console.log("inRenderBooks");
+  const main = document.querySelector('main');
   json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
+    const h2 = document.createElement('h2');
+    h2.innerHTML = `<h2>${book.name}</h2>`;
+    main.appendChild(h2);
+    console.log("1");
   })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  console.log("grrrrrr");
-  let x = fetchBooks();
-  renderBooks(x);
+  fetchBooks();
 })
