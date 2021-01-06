964289545de4fb82b8e57eca5c6d7a0a78404fbf
SVRourke <samuel@rourke.tech>
2021-01-05 22:32:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
-  .then(resp => resp.json());
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
   .then(data => renderBooks(data));
 
 }
