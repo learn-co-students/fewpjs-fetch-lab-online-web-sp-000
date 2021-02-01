db7681806433bedd01da08f35f227c566108cc96
anpham148 <anpham148@gmail.com>
2021-02-01 17:15:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function fetchBooks() {
-
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => console.log(renderBooks(json)));
+return fetch('https://anapioficeandfire.com/api/books')
 }
+
+function fetch5thBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => fifthBook(json));
+}
+
 
 function renderBooks(json) {
   const main = document.querySelector('main')
@@ -14,3 +24,19 @@ function renderBooks(json) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
+function fifthBook(json) {
+  const main = document.querySelector('main')
+  const h3 = document.createElement('h3')
+  json[4].characters.forEach(character => {
+    let str = "1031";
+    let match = character.slice(-4);
+    if (str === match) {
+      h3.innerHTML = character
+    };
+  })
+
+  main.appendChild(h3)
+}
+
+
