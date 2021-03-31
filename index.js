abc03242eb48886cfd700d9e3d88a3c601771475
amcmullen9291 <amcmullen9291@gmail.com>
2021-03-31 02:13:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  return fetch("https://anapioficeandfire.com/api/books")
+  return fetch ('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => renderBooks(json))
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(books) {
@@ -11,6 +11,7 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+    console.log(book.name)
   });
 }
 
